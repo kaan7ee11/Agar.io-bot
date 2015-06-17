a3fbead7066c6e9c33a11ed81eb60fffcccfbea1
kaan7ee11 <kaan@gmail.com>
2015-06-17 20:33:37 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -884,7 +884,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  originalName = "NotReallyABot",
+  originalName = "kaan7ee11",
   sessionScore = 0,
   serverIP = "",
   interNodes = [],
